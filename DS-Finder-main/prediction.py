d34da1bd8f59ed1259e5352380243f6247102cfa
tangwei-pku <tangwei@stu.pku.edu.cn>
2024-12-24 20:40:42 +0800

    remove StandardScaler

--- DS-Finder-main/prediction.py
+++ DS-Finder-main/prediction.py
@@ -4,7 +4,6 @@
 import argparse
 import pickle
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 def load_model(model_path):
     """Load the model from the specified file"""
@@ -24,12 +23,9 @@ def check_column_names(df, required_columns):
     return True
 
 def preprocess_data(df, columns_to_scale):
-    """Preprocess the data by filling NaN values and scaling numerical columns"""
+    """Preprocess the data by filling NaN values"""
     df[columns_to_scale] = df[columns_to_scale].fillna(0)
-    std_scaler = StandardScaler()
     X = df[columns_to_scale]
-    numeric_columns = X.select_dtypes(include=['float64', 'int64']).columns
-    X[numeric_columns] = std_scaler.fit_transform(X[numeric_columns])
     return X
 
 def main(model_path, dataset_path, output_path):
