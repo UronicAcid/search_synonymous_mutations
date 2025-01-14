37ddb2259d968cef49b0cddb85747f967775e71d
tangwei-pku <tangwei@stu.pku.edu.cn>
2025-01-14 16:47:12 +0800

    update KYSE-30

--- app.py
+++ app.py
@@ -74,6 +74,8 @@ def search_page():
     # 使用selectbox代替text_input让用户选择基因名称
     gene_name = st.selectbox("Enter Gene Name", options=unique_genes)
     cell_line = st.selectbox("Select Cell Line", ["HCT116",  "KYSE-30", "A549"])
+    if cell_line == "KYSE-30":
+        cell_line = "KYSE30"
     filtered_df = df[(df["gene"] == gene_name) & (df["cell_line"] == cell_line)]
     st.write(" ")
     # 添加一个按钮来控制是否显示结果和下载链接
